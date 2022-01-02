e856fc0e133f6bdc4b30e6b880bee60f7fe75a98
VovanVovanich <38005493+VovanVovanich@users.noreply.github.com>
2022-01-02 11:08:01 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "web" {
 yum -y update
 yum -y install httpd
 myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
-echo "<h2>PROD WebServer with IP: $myip</h2><br>Build by Terraform!"  >  /var/www/html/index.html
+<body bgcolor="black">
 echo "<h2 color="blue">${var.server_name}-WebServer with IP: $myip</h2><br><font color="red">Build by Terraform and Vovik!"  >  /var/www/html/index.html
 sudo service httpd start
 chkconfig httpd on
